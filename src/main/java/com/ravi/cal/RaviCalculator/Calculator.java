cdc3b62430f6d2e284243dccbdd068fb6cf77c64
Cloud User <centos@ip-192-168-4-120.ec2.internal>
2020-01-08 16:20:22 +0000

    changes in code

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -31,7 +31,7 @@ public class Calculator  {
 	
 	public long subFucn(long first, long second){
 		
-		return second-first;
+
 	}
 	
 	public long mulFucn(long first, long second){
@@ -53,7 +53,6 @@ public class Calculator  {
 		long second = sc.nextLong();
 		cc.setSecond(second);
 		
-		System.out.println("The sum of two numbers is: "+cc.addFucn(first, second));
 		System.out.println("The difference of two numbers is: "+cc.subFucn(first, second));
 		System.out.println("The multiples of two numbers is: "+cc.mulFucn(first, second));
 		
